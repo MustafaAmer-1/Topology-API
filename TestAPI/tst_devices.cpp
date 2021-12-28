77ac9bc6638916fb932f89c756878fc9c6334cc5
Mustafa Amer <1806185@eng.asu.edu.eg>
2021-12-28 20:37:55 +0200

    remove device test

--- TestAPI/tst_devices.cpp
+++ TestAPI/tst_devices.cpp
@@ -12,35 +12,21 @@ public:
     DevicesTest();
     ~DevicesTest();
 private:
-    Device* device;
     Resistor* resistor;
 private slots:
-    void device_test();
     void resistor_test();
 };
 
 DevicesTest::DevicesTest(){
-    device = new Device();
     resistor = new Resistor();
 }
 
 DevicesTest::~DevicesTest(){
-    delete device;
     delete resistor;
 }
 
-void DevicesTest::device_test(){
-    QCOMPARE(device->getId(), "0");
-    device = new Device(5154, 6654, 653);
-    QCOMPARE(device->getDefault(), 5154);
-    QCOMPARE(device->getMin(), 6654);
-    QCOMPARE(device->getMax(), 653);
-    device->setId("4545");
-    QCOMPARE(device->getId(), "4545");
-}
-
 void DevicesTest::resistor_test() {
-    QCOMPARE(resistor->getId(), "1");
+    QCOMPARE(resistor->getId(), "0");
     resistor = new Resistor(5154, 6654, 653);
     QCOMPARE(resistor->getDefault(), 5154);
     QCOMPARE(resistor->getMin(), 6654);
